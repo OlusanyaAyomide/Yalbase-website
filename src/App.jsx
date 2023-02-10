54846a746b877a393b1fb6592edd171c82ce6510
onotaizee@gmail.com <onotaizee@gmail.com>
2023-02-10 21:03:31 +0100

    +added: landing page bottom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Bottom from "./component/Bottom"
 import Header from "./component/Header"
 import Hero from "./component/Hero"
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="text-white" >
      <Header/>
      <Hero/>
+     <Bottom/>
     </div>
   )
 }
